f9da5b810eea91244ad8d736b92fe6fc198bf6cc
jo030225 <whwngur123@naver.com>
2020-12-11 12:23:11 +0900

    회원가입 구현

--- LaptopApp/LaptopApp/SignUpViewController.swift
+++ LaptopApp/LaptopApp/SignUpViewController.swift
@@ -6,10 +6,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class SignUpViewController: UIViewController {
 
     @IBOutlet weak var done: UIButton!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var id: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    var nameData: String = ""
+    var idData: String = ""
+    var passwordData: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,22 +26,42 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func testJson(name: String, id: String, password: String) {
+        let URL = "http://192.168.137.1:3000/post-server"
+        
+        
+        let PARAM: Parameters = [
+            "name": name,
+            "id": id,
+            "password": password,
+        ]
+        //위의 URL와 파라미터를 담아서 POST 방식으로 통신하며, statusCode가 200번대(정상적인 통신) 인지 유효성 검사 진행
+        let alamo = AF.request(URL, method: .post, parameters: PARAM).validate(statusCode: 200..<300)
+        //결과값으로 문자열을 받을 때 사용
+        alamo.responseString() { response in
+            switch response.result
+            {
+            //통신성공
+            case .success(let value):
+                print("value: \(value)")
+            //통신실패
+            case .failure(let error):
+                print("error: \(String(describing: error.errorDescription))")
+                //  self.resultLabel.text = "\(error)"
+                print("\(error)")
+            }
+        }
+        
     }
-    */
-
+    
     @IBAction func cancleBtn(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     @IBAction func doneBtn(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-        
+        nameData = name.text!
+        idData = id.text!
+        passwordData = password.text!
+        testJson(name: nameData, id: idData, password: passwordData)
     }
 }
